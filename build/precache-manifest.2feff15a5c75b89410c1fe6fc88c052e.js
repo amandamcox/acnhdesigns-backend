4e15cff178755c110724ca9284a8e4bb41b01819
Amanda Cox <amandamcox@gmail.com>
2020-05-26 12:48:38 -0700

    Adding static build folder for production

---
+++ build/precache-manifest.2feff15a5c75b89410c1fe6fc88c052e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7ac21d3e27c6216f26a0b6a85107f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f475225f1ecb480d8189",
+    "url": "/static/css/main.27ef29ed.chunk.css"
+  },
+  {
+    "revision": "6c9e0699e7b60cf27891",
+    "url": "/static/js/2.ba3be294.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ba3be294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f475225f1ecb480d8189",
+    "url": "/static/js/main.c7fbbb82.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);

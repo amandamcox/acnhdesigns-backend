409c603da8f86c3b29842b7478e856bc60724b3a
Amanda Cox <amandamcox@gmail.com>
2020-06-03 15:31:46 -0700

    Added /api/designs/search endpoint to search in designName

--- api/components/design/routes.js
+++ api/components/design/routes.js
@@ -2,6 +2,7 @@ const designRouter = require('express').Router()
 const {
 	getDesigns,
 	getDesignsForUser,
+	getDesignsBySearch,
 	createNewDesign,
 	bulkNewDesigns,
 	deleteDesign,
@@ -13,6 +14,7 @@ const { upload } = require('../../../config/awsS3Config')
 
 designRouter.get('/', getDesigns)
 designRouter.get('/user', getDesignsForUser)
+designRouter.get('/search', getDesignsBySearch)
 designRouter.post('/', createNewDesign)
 designRouter.post('/upload', upload.single('designImage'), uploadImage)
 designRouter.post('/bulk', bulkNewDesigns)

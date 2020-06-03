15f675ccab030ddfc3909b12b57c028527169976
Amanda Cox <amandamcox@gmail.com>
2020-06-03 16:15:47 -0700

    Deploying newest code

---
+++ build/precache-manifest.a9127b7789e1947ac71d9cd10be067c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be9d44cd1a29ec3d53f30127085ca9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e5d2747d78cc176687d",
+    "url": "/static/css/main.eed97a42.chunk.css"
+  },
+  {
+    "revision": "af564ede4b46fdc6c472",
+    "url": "/static/js/2.7e04fa6b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7e04fa6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e5d2747d78cc176687d",
+    "url": "/static/js/main.bb03be86.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
